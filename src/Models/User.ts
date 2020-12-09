4e6bb0e049b33528ea0f56ddbf22e5e456cc18d0
Bruno Henrique Santos <brunohenriquesantos272@gmail.com>
2020-12-09 19:26:26 +0000

    Adiciona o Model User

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -1,1 +1,17 @@
-import {} from '@typegoose/typegoose';
+import { prop, getModelForClass } from '@typegoose/typegoose';
+
+class UserModel {
+  @prop()
+  public name: string;
+
+  @prop({ unique: true })
+  public email: string;
+
+  @prop({ select: true })
+  public password: string;
+
+  @prop({ required: true, default: Date.now() })
+  public created_at: Date;
+}
+
+export const User = getModelForClass(UserModel);
