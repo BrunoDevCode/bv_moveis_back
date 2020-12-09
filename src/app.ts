2b76bf213399cdf960fe37bef52c16aec20251ee
Bruno Henrique Santos <brunohenriquesantos272@gmail.com>
2020-12-09 18:05:59 +0000

    Initial Configs

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import routes from './routes';
+import morgan from 'morgan';
+import { connect } from 'mongoose';
+import { MONGO_URI } from './config/env';
+
+connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(morgan('Dev'));
+app.use(routes);
 
 export default app;
