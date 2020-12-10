1be5d28ab5fdbbc5fe0eef2eb7576036dbf55d01
Bruno Henrique <brunohenriquesantos272@gmail.com>
2020-12-09 23:35:13 -0300

    Add catch all errors function, static files and log DB Connect

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,36 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import { connect } from 'mongoose';
 import routes from './routes';
 import { MONGO_URI } from './config/env';
+import { resolve } from 'path';
 
-connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('> DB Connect sucessful');
+  });
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan('Dev'));
+app.use(morgan('dev'));
+
+app.use('/files', express.static(resolve(__dirname, '..', 'tmp', 'uploads')));
+
 app.use(routes);
+
+app.use((request, response, next) => {
+  const error = new Error('Not found (404)');
+  error.status = 404;
+  next(error);
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  response.status(error.status || 500);
+  response.json({ Error: error.message });
+});
 
 export default app;
