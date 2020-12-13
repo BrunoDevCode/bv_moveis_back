bacdd35b6a8e16fc126b8c908d6271c492d9ce90
Bruno Henrique <brunohenriquesantos272@gmail.com>
2020-12-13 01:18:43 -0300

    Config of uploads

---
+++ src/config/multer.ts
@@ -0,0 +1,64 @@
+import multer from 'multer';
+import { resolve } from 'path';
+import { randomBytes } from 'crypto';
+import aws from 'aws-sdk';
+import multerS3 from 'multer-s3';
+
+import { BUCKET_NAME, STORAGE_TYPE } from '../config/env';
+
+const MAX_SIZE = 2 * 1024 * 1024;
+
+const storageTypes = {
+  local: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, resolve(__dirname, '..', '..', 'tmp', 'uploads'));
+    },
+    filename: (req, file, cb) => {
+      randomBytes(16, (err, hash) => {
+        if (err) cb(err);
+
+        file.key = `${hash.toString('hex')}-${file.originalname}`;
+
+        cb(null, file.key);
+      });
+    },
+  }),
+
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (req, file, cb) => {
+      randomBytes(16, (err, hash) => {
+        if (err) cb(err);
+
+        const filename = `${hash.toString('hex')}-${file.originalname}`;
+
+        cb(null, filename);
+      });
+    },
+  }),
+}
+
+export default {
+  dest: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+  storage: storageTypes[STORAGE_TYPE],
+  limits: {
+    fileSize: MAX_SIZE,
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = [
+      'image/jpeg',
+      'image/pjpeg',
+      'image/png',
+      'image/gif'
+    ];
+
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Invalid file type!'));
+    }
+  },
+};
