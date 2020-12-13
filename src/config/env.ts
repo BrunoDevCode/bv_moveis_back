b6c9074ee6badf9500c6273296ea7376e1f76cfc
Bruno Henrique <brunohenriquesantos272@gmail.com>
2020-12-13 01:18:03 -0300

    Adding new varible envs

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,4 +3,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 dotenv.config({ path: '../../.env' });
 
-export const { PORT, MONGO_URI }: any = process.env;
+export const { PORT, MONGO_URI, BUCKET_NAME, STORAGE_TYPE, APP_URL }: any = process.env;
