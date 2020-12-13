01854cfa1e858e9b2145210acd42ad4b1e6c3155
Bruno Henrique <brunohenriquesantos272@gmail.com>
2020-12-13 01:19:15 -0300

    Route of upload

--- src/Controllers/AdminControllers/ItemsController.ts
+++ src/Controllers/AdminControllers/ItemsController.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from 'express';
+import { Item } from '../../Models/Item';
+import { Image } from '../../Models/Image';
 
 export default class ItemsController {
-  async create(request:Request, response: Response) {
-    
+  async create(request: Request, response: Response) {
+    const { originalname: name, size, key, location: url = "" } = request.file;
+    // const { title, description, inStock} = request.body;
+
+    const imageSave = await Image.create({
+      name,
+      size,
+      key,
+      url
+    });
+
+    return response.send(imageSave);
   }
 }
