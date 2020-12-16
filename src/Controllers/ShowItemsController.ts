73ef42a427ae8f461a7c930485fa47d23d78fa9b
Bruno Henrique <brunohenriquesantos272@gmail.com>
2020-12-16 19:29:53 -0300

    Show the items to homepage or all items with one image

--- src/Controllers/ShowItemsController.ts
+++ src/Controllers/ShowItemsController.ts
@@ -1,8 +1,26 @@
-import { Request, Response } from "express";
-import { Item } from '../Models/Item';
+import { Request, Response, NextFunction } from 'express';
+import Item from '../Models/Item';
 
 export default class ShowItemsController {
-  async index(request: Request, response: Response) {
-    
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      const homepageItems = await Item.find({ isHomepage: true })
+        .limit(6)
+        .populate('images');
+
+      return response.status(200).json(homepageItems);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async show(request: Request, response: Response) {
+    const arrayOfItems = await Item.find().populate({
+      path: 'images',
+      select: 'url',
+      options: { limit: 1 },
+    });
+
+    return response.status(200).json(arrayOfItems);
   }
 }
