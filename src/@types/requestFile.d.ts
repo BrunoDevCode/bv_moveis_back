d14a36f491014d620b3bbd75abb0b310278ed84b
Bruno Henrique <brunohenriquesantos272@gmail.com>
2020-12-16 19:25:06 -0300

    Tipagem das rotas com request personalidado

--- src/@types/requestFile.d.ts
+++ src/@types/requestFile.d.ts
@@ -1,10 +1,15 @@
 import { Request } from 'express';
 
 export default interface RequestFile extends Request {
+  body: {
+    itemID: string;
+  }
+
   file: {
-    originalname?: string;
-    key?: string;
-    location?: string;
+    originalname: string;
+    name: string;
+    key: string;
+    location: string;
     size: number;
   }
-}
+};
