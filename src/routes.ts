6fba55f286983b8946bb3cf24f0d1347a2bd1efd
Bruno Henrique <brunohenriquesantos272@gmail.com>
2020-12-16 19:31:32 -0300

    Add new routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,17 +2,35 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
+import ShowItemsController from './Controllers/ShowItemsController';
+import ShowItemController from './Controllers/ShowItemController';
+
 import AuthController from './Controllers/AdminControllers/AuthController';
-import ItemsController from './Controllers/AdminControllers/ItemsController';
+import ItemController from './Controllers/AdminControllers/ItemController';
+import ImagesController from './Controllers/AdminControllers/ImagesController';
+
+import TokenMiddleware from './Middleware/TokenMiddleware';
 
 const routes = Router();
 
+const showItemsController = new ShowItemsController();
+const showItemController = new ShowItemController();
 const authController = new AuthController();
-const itemsController = new ItemsController();
+const itemController = new ItemController();
+const imagesController = new ImagesController();
+
+routes.get('/items/homepage', showItemsController.index);
+routes.get('/items', showItemsController.show);
+routes.get('/item/:itemID', showItemController.index);
 
 routes.post('/admin/login', authController.index);
 routes.post('/admin/register', authController.create);
 
-routes.post('/item/create', multer(multerConfig).single("file"), itemsController.create);
+routes.post('/admin/item/create', TokenMiddleware, itemController.create);
+
+routes.post('/admin/image/upload', TokenMiddleware, multer(multerConfig).single('file'), imagesController.create);
+routes.delete('/admin/image/delete/:imageID', TokenMiddleware, imagesController.destroy);
+
+routes.delete('/admin/item/delete/:itemID', TokenMiddleware, itemController.destroy);
 
 export default routes;
